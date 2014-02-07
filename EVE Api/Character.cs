a184dcac4f055daf0160d38cb3c4c8f8e4927c0c
ezet <lars.dahl@gmail.com>
2014-02-07 20:54:06 +0100

    Initial commit.

---
+++ EVE Api/Character.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eZet.Eve.EveApi
+{
+    public class Character {
+
+        private string uriBase = "https://api.eveonline.com";
+        public int KeyId{ get; private set; }
+
+        public string KeyCode { get; private set; }
+
+        public int CharId { get; private set; }
+
+        public Character(int keyId, string keyCode, int charId) {
+            this.KeyId = keyId;
+            this.KeyCode = keyCode;
+            this.CharId = charId;
+        }
+
+        public void getWalletJournal() {
+            string uri = "/char/WalletJournal.xml.aspx";
+            Stream stream;
+            var req = (HttpWebRequest)WebRequest.Create(uriBase + uri);
+            try {
+                using (HttpWebResponse response = (HttpWebResponse)req.GetResponse()) {
+                    if (response.StatusCode == HttpStatusCode.OK) {
+                        stream = response.GetResponseStream();
+                    }
+                }
+            } catch (Exception e) {
+
+            }
+        }
+
+        public string createUri(params string[] args) {
+
+
+            return "";
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ EVE Api/EveApi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eZet.Eve.EveApi {
+    public class EveApi {
+
+        private static int keyId = 3053778;
+
+        private static string keyCode = "Hu3uslqNc3HDP8XmMMt1Cgb56TpPqqnF2tXssniROFkIMEDLztLPD8ktx6q5WVC2";
+
+        private static int charId = 0;
+
+        public int KeyId { get; private set; }
+
+        public string KeyCode { get; private set; }
+
+        public int CharId { get; private set; }
+
+        public Character Character { get; private set; }
+
+        public EveApi(int keyId, string keyCode, int charId) {
+            this.KeyId = keyId;
+            this.KeyCode = keyCode;
+            this.CharId = charId;
+            Character = new Character(KeyId, KeyCode, CharId);
+
+        }
+
+
+        static public void Main() {
+            var api = new EveApi(keyId, keyCode, charId);
+
+            api.Character.getWalletJournal();
+        }
+
+
+    }
+}
