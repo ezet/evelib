dae1f89b31e9509112df61285586463f0acb4b20
ezet <lars.dahl@gmail.com>
2014-05-05 14:25:38 +0200

    Added Faction and Alliance data.

--- EveLib.EveOnline/Models/Account/ApiKeyInfo.cs
+++ EveLib.EveOnline/Models/Account/ApiKeyInfo.cs
@@ -34,17 +34,30 @@ namespace eZet.EveLib.Modules.Models.Account {
         [Serializable]
         [XmlRoot("row", IsNullable = false)]
         public class CharacterInfo {
-            [XmlAttribute("characterName")]
-            public string CharacterName { get; set; }
 
             [XmlAttribute("characterID")]
             public long CharacterId { get; set; }
 
-            [XmlAttribute("corporationName")]
-            public string CorporationName { get; set; }
+            [XmlAttribute("characterName")]
+            public string CharacterName { get; set; }
 
             [XmlAttribute("corporationID")]
             public long CorporationId { get; set; }
+
+            [XmlAttribute("corporationName")]
+            public string CorporationName { get; set; }
+
+            [XmlAttribute("allianceID")]
+            public long AllianceId { get; set; }
+
+            [XmlAttribute("allianceName")]
+            public string AllianceName { get; set; }
+
+            [XmlAttribute("factionID")]
+            public long FactionId { get; set; }
+
+            [XmlAttribute("factionName")]
+            public string FactionName { get; set; }
         }
     }
 }
