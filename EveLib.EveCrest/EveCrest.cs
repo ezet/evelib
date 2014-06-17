ce4748f2982fbbf4bd1905fe5279f125de160e4a
ezet <lars.dahl@gmail.com>
2014-06-17 14:48:22 +0200

    Added proper comments to public EveCrest.

--- EveLib.EveCrest/EveCrest.cs
+++ EveLib.EveCrest/EveCrest.cs
@@ -6,92 +6,205 @@ using eZet.EveLib.Modules.Models;
 namespace eZet.EveLib.Modules {
     public class EveCrest {
 
+        /// <summary>
+        /// The default URI used to access the CREST API.
+        /// </summary>
         public const string DefaultUri = "http://public-crest.eveonline.com/";
 
+        /// <summary>
+        /// Creates a new EveCrest object with a default request handler
+        /// </summary>
         public EveCrest() {
             RequestHandler = new RequestHandler(new HttpRequester(), new DynamicJsonSerializer());
             BaseUri = new Uri(DefaultUri);
         }
 
+        /// <summary>
+        /// Gets or sets the request handler used by this instance
+        /// </summary>
         public IRequestHandler RequestHandler { get; set; }
 
-        public ISerializer Serializer { get; set; }
-
+        /// <summary>
+        /// Gets or sets the URI used to access the CREST API by this instance.
+        /// </summary>
         public Uri BaseUri { get; set; }
 
+        /// <summary>
+        /// Returns data on the specified killmail.
+        /// Path: /killmails/$warId/$hash/
+        /// </summary>
+        /// <param name="id">Killmail ID</param>
+        /// <param name="hash">Killmail hash</param>
+        /// <returns>Returns data for the specified killmail.</returns>
         public Killmail GetKillmail(long id, string hash) {
             return GetKillmailAsync(id, hash).Result;
         }
 
+        /// <summary>
+        /// Returns data on the specified killmail.
+        /// Path: /killmails/$warId/$hash/
+        /// </summary>
+        /// <param name="id">Killmail ID</param>
+        /// <param name="hash">Killmail hash</param>
+        /// <returns>Returns data for the specified killmail.</returns>
         public Task<Killmail> GetKillmailAsync(long id, string hash) {
             string relPath = "killmails/" + id + "/" + hash + "/";
             return requestAsync<Killmail>(relPath);
         }
 
+        /// <summary>
+        /// Returns a list of all active incursions.
+        /// Path: /incursions/
+        /// </summary>
+        /// <returns>A list of all active incursions.</returns>
         public IncursionCollection GetIncursions() {
             return GetIncursionsAsync().Result;
         }
 
+        /// <summary>
+        /// Returns a list of all active incursions.
+        /// Path: /incursions/
+        /// </summary>
+        /// <returns>A list of all active incursions.</returns>
         public Task<IncursionCollection> GetIncursionsAsync() {
             const string relPath = "incursions/";
             return requestAsync<IncursionCollection>(relPath);
         }
 
+        /// <summary>
+        /// Returns a list of all alliances.
+        /// Path: /alliances/
+        /// </summary>
+        /// <param name="page">The 1-indexed page to return. Number of total pages is available in the repsonse.</param>
+        /// <returns>A list of all alliances.</returns>
         public AllianceCollection GetAlliances(int page = 1) {
             return GetAlliancesAsync(page).Result;
         }
 
+        /// <summary>
+        /// Returns a list of all alliances.
+        /// Path: /alliances/
+        /// </summary>
+        /// <param name="page">The 1-indexed page to return. Number of total pages is available in the repsonse.</param>
+        /// <returns>A list of all alliances.</returns>
         public Task<AllianceCollection> GetAlliancesAsync(int page = 1) {
             string relPath = "alliances/?page=" + page;
             return requestAsync<AllianceCollection>(relPath);
         }
 
+        /// <summary>
+        /// Returns data about a specific alliance.
+        /// Path: /alliances/$allianceId/
+        /// </summary>
+        /// <param name="allianceId">A valid alliance ID</param>
+        /// <returns>Data for specified alliance</returns>
         public Alliance GetAlliance(long allianceId) {
             return GetAllianceAsync(allianceId).Result;
         }
 
+        /// <summary>
+        /// Returns data about a specific alliance.
+        /// Path: /alliances/$allianceId/
+        /// </summary>
+        /// <param name="allianceId">A valid alliance ID</param>
+        /// <returns>Data for specified alliance</returns>
         public Task<Alliance> GetAllianceAsync(long allianceId) {
             string relPath = "alliances/" + allianceId + "/";
             return requestAsync<Alliance>(relPath);
         }
 
+        /// <summary>
+        /// Returns daily price and volume history for a specific region and item type.
+        /// Path: /market/$regionId/types/$typeId/history/
+        /// </summary>
+        /// <param name="regionId">Region ID</param>
+        /// <param name="typeId">Type ID</param>
+        /// <returns>Market history for the specified region and type.</returns>
         public MarketHistoryCollection GetMarketHistory(int regionId, int typeId) {
             return GetMarketHistoryAsync(regionId, typeId).Result;
         }
 
-
+        /// <summary>
+        /// Returns daily price and volume history for a specific region and item type.
+        /// Path: /market/$regionId/types/$typeId/history/
+        /// </summary>
+        /// <param name="regionId">Region ID</param>
+        /// <param name="typeId">Type ID</param>
+        /// <returns>Market history for the specified region and type.</returns>
         public Task<MarketHistoryCollection> GetMarketHistoryAsync(int regionId, int typeId) {
             string relPath = "market/" + regionId + "/types/" + typeId + "/history/";
             return requestAsync<MarketHistoryCollection>(relPath);
         }
 
+        /// <summary>
+        /// Returns a list of all wars.
+        /// Path: /wars/
+        /// </summary>
+        /// <param name="page">The 1-indexed page to return. Number of total pages is available in the repsonse.</param>
+        /// <returns>A list of all wars.</returns>
         public WarCollection GetWars(int page = 1) {
             return GetWarsAsync().Result;
         }
 
+        /// <summary>
+        /// Returns a list of all wars.
+        /// Path: /wars/
+        /// </summary>
+        /// <param name="page">The 1-indexed page to return. Number of total pages is available in the repsonse.</param>
+        /// <returns>A list of all wars.</returns>
         public Task<WarCollection> GetWarsAsync(int page = 1) {
             string relPath = "/wars/?page=" + page;
             return requestAsync<WarCollection>(relPath);
         }
 
-        public War GetWar(int id) {
-            return GetWarAsync(id).Result;
+        /// <summary>
+        /// Returns data for a specific war.
+        /// Path: /wars/$warId
+        /// </summary>
+        /// <param name="warId">War ID</param>
+        /// <returns>Data for the specified war.</returns>
+        public War GetWar(int warId) {
+            return GetWarAsync(warId).Result;
         }
 
-        public Task<War> GetWarAsync(int id) {
-            string relPath = "/wars/" + id + "/";
+        /// <summary>
+        /// Returns data for a specific war.
+        /// Path: /wars/$warId
+        /// </summary>
+        /// <param name="warId">War ID</param>
+        /// <returns>Data for the specified war.</returns>
+        public Task<War> GetWarAsync(int warId) {
+            string relPath = "/wars/" + warId + "/";
             return requestAsync<War>(relPath);
         }
 
-        public KillmailCollection GetWarKillmails(int id) {
-            return GetWarKillmailsAsync(id).Result;
+        /// <summary>
+        /// Returns a list of all killmails related to a specified war.
+        /// Path: /wars/$warId/killmails/all
+        /// </summary>
+        /// <param name="warId">War ID</param>
+        /// <returns>A list of all killmails related to the specified war.</returns>
+        public KillmailCollection GetWarKillmails(int warId) {
+            return GetWarKillmailsAsync(warId).Result;
         }
 
-        public Task<KillmailCollection> GetWarKillmailsAsync(int id) {
-            string relPath = "/wars/" + id + "/killmails/all/";
+        /// <summary>
+        /// Returns a list of all killmails related to a specified war.
+        /// Path: /wars/$warId/killmails/all
+        /// </summary>
+        /// <param name="warId">War ID</param>
+        /// <returns>A list of all killmails related to the specified war.</returns>
+        public Task<KillmailCollection> GetWarKillmailsAsync(int warId) {
+            string relPath = "/wars/" + warId + "/killmails/all/";
             return requestAsync<KillmailCollection>(relPath);
         }
 
+        /// <summary>
+        /// Performs a request using the request handler.
+        /// </summary>
+        /// <typeparam name="T">Response type</typeparam>
+        /// <param name="relPath">Relative path</param>
+        /// <returns></returns>
         private Task<T> requestAsync<T>(string relPath) {
             return RequestHandler.RequestAsync<T>(new Uri(BaseUri, relPath));
         }
