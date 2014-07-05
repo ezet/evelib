fff6433917e118957f0ba904e999950ed5624f21
ezet <lars.dahl@gmail.com>
2014-07-05 12:56:04 +0200

    Crest industry systems.

---
+++ EveLib.EveCrest/Models/CrestIndustrySystems.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace eZet.EveLib.Modules.Models {
+    /// <summary>
+    /// Represents the response for Industry Systems in CREST
+    /// </summary>
+    [DataContract]
+    public class CrestIndustrySystems : CrestCollectionResponse {
+
+        /// <summary>
+        /// The solar system
+        /// </summary>
+        [DataMember(Name = "solarSystem")]
+        public List<SolarSystemEntry> SolarSystems { get; set; }
+
+        /// <summary>
+        /// Represesents a solar system for a SystemCostEntry
+        /// </summary>
+        [DataContract]
+        public class SolarSystemEntry {
+
+            /// <summary>
+            /// The solar system
+            /// </summary>
+            public CrestNamedEntity SolarSystem { get; set; }
+
+            /// <summary>
+            /// A list of system costs
+            /// </summary>
+            public List<SystemCostEntry> SystemCostIndices { get; set; }
+
+        }
+
+        /// <summary>
+        /// Represents a system cost index in CREST Industry Systems collection
+        /// </summary>
+        [DataContract]
+        public class SystemCostEntry : CrestNamedEntity {
+            /// <summary>
+            /// The cost index
+            /// </summary>
+            [DataMember(Name = "costIndex")]
+            public float CostIndex { get; set; }
+
+            /// <summary>
+            /// The activity ID
+            /// </summary>
+            [DataMember(Name = "activityID")]
+            public int ActivityId { get; set; }
+
+            /// <summary>
+            /// The activity name
+            /// </summary>
+            [DataMember(Name = "activityName")]
+            public string ActivityName { get; set; }
+
+        }
+    }
+}
