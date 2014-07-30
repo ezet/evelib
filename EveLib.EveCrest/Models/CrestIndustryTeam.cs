3ad7af32341b871f863e2a106d41646c22f835b9
ezet <lars.dahl@gmail.com>
2014-07-30 12:27:31 +0200

    Added name and activity to industry teams.

---
+++ EveLib.EveCrest/Models/CrestIndustryTeam.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace eZet.EveLib.Modules.Models {
+    /// <summary>
+    /// Represents a CREST industry team
+    /// </summary>
+    public class CrestIndustryTeam {
+
+        /// <summary>
+        /// The solar system
+        /// </summary>
+        [DataMember(Name = "solarSystem")]
+        public CrestNamedEntity SolarSystem { get; set; }
+
+        /// <summary>
+        /// The team specialization
+        /// </summary>
+        [DataMember(Name = "specialization")]
+        public CrestLinkedEntity Specialization { get; set; }
+
+        /// <summary>
+        /// The team creation time
+        /// </summary>
+        [DataMember(Name = "creationTime")]
+        public DateTime CreationTime { get; set; }
+
+        /// <summary>
+        /// The team expiry time
+        /// </summary>
+        [DataMember(Name = "expiryTime")]
+        public DateTime ExpiryTime { get; set; }
+
+        /// <summary>
+        /// The team cost modifier
+        /// </summary>
+        [DataMember(Name = "costModifier")]
+        public float CostModifier { get; set; }
+
+        /// <summary>
+        /// A list of the team workers
+        /// </summary>
+        [DataMember(Name = "workers")]
+        public IList<CrestIndustryTeamWorker> Workers { get; set; }
+
+        /// <summary>
+        /// The team name
+        /// </summary>
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Activity
+        /// </summary>
+        [DataMember(Name = "activity")]
+        public int Activity { get; set; }
+
+    }
+}
--- EveLib.EveCrest/Models/CrestIndustryTeams.cs
+++ EveLib.EveCrest/Models/CrestIndustryTeams.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Runtime.Serialization;
 
 namespace eZet.EveLib.Modules.Models {
@@ -13,50 +12,7 @@ namespace eZet.EveLib.Modules.Models {
         /// A list of the teams
         /// </summary>
         [DataMember(Name = "items")]
-        public IList<Team> Teams { get; set; }
-
-        /// <summary>
-        /// Represents a CREST industry team
-        /// </summary>
-        public class Team {
-
-            /// <summary>
-            /// The solar system
-            /// </summary>
-            [DataMember(Name = "solarSystem")]
-            public CrestNamedEntity SolarSystem { get; set; }
-
-            /// <summary>
-            /// The team specialization
-            /// </summary>
-            [DataMember(Name = "specialization")]
-            public CrestLinkedEntity Specialization { get; set; }
-
-            /// <summary>
-            /// The team creation time
-            /// </summary>
-            [DataMember(Name = "creationTime")]
-            public DateTime CreationTime { get; set; }
-
-            /// <summary>
-            /// The team expiry time
-            /// </summary>
-            [DataMember(Name = "expiryTime")]
-            public DateTime ExpiryTime { get; set; }
-
-            /// <summary>
-            /// The team cost modifier
-            /// </summary>
-            [DataMember(Name = "costModifier")]
-            public float CostModifier { get; set; }
-
-            /// <summary>
-            /// A list of the team workers
-            /// </summary>
-            [DataMember(Name = "workers")]
-            public IList<CrestIndustryTeamWorker> Workers { get; set; }
-
-        }
+        public IList<CrestIndustryTeam> Teams { get; set; }
    
     }
 }
