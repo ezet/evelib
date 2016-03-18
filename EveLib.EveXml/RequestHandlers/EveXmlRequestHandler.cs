4303ac05486bb5f2a503e9e73d9b56c5707ae79c
ezet <lars.dahl@gmail.com>
2016-03-18 11:56:41 +0100

    Fixed some things in RequestHandler

--- EveLib.EveXml/RequestHandlers/EveXmlRequestHandler.cs
+++ EveLib.EveXml/RequestHandlers/EveXmlRequestHandler.cs
@@ -47,12 +47,18 @@ namespace eZet.EveLib.EveXmlModule.RequestHandlers {
             if (cached) return Serializer.Deserialize<T>(data);
             if (CacheLevel == CacheLevel.CacheOnly) return default(T);
             try {
-                data = await HttpRequestHelper.RequestAsync(uri).ConfigureAwait(false);
+                var response = await HttpRequestHelper.GetResponseAsync(HttpRequestHelper.CreateRequest(uri)).ConfigureAwait(false);
+                // special handling for status code 000, because we cannot fetch the response body
+                if (response.StatusCode.ToString() == "0") {
+                    throw new EveXmlException("Kill log exhausted (You can only fetch kills that are less than a month old)", 119, new WebException("Unknown", null, WebExceptionStatus.UnknownError, response));
+                }
+                data = await HttpRequestHelper.GetResponseContentAsync(response);
+
             }
             catch (WebException e) {
                 _trace.TraceEvent(TraceEventType.Error, 0, "Http Request failed");
                 var response = (HttpWebResponse) e.Response;
-                if (response == null) throw;
+                if (response == null) throw new EveXmlException("Unknown", 0, e);
                 var responseStream = response.GetResponseStream();
                 if (responseStream == null) throw;
                 using (var reader = new StreamReader(responseStream)) {
